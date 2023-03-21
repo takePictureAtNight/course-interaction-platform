577f26ffbed4c8614fbc1ed65adaaef10db10a03
junfeng <qzhujf1012@qq.com>
2023-03-21 17:58:04 +0800

    66666

--- course-resourse/src/main/java/com/peking/courseresourse/service/impl/ProductDesignServiceImpl.java
+++ course-resourse/src/main/java/com/peking/courseresourse/service/impl/ProductDesignServiceImpl.java
@@ -1,7 +1,11 @@
 package com.peking.courseresourse.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import dto.UserDTO;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +15,7 @@ import utils.Query;
 import com.peking.courseresourse.dao.ProductDesignDao;
 import com.peking.courseresourse.entity.ProductDesignEntity;
 import com.peking.courseresourse.service.ProductDesignService;
+import utils.UserHolder;
 
 
 @Service("productDesignService")
@@ -18,9 +23,16 @@ public class ProductDesignServiceImpl extends ServiceImpl<ProductDesignDao, Prod
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        UserDTO user = UserHolder.getUser();
+        LambdaQueryWrapper<ProductDesignEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper//.eq(ProductDesignEntity::getCreateBy,user.getId())
+                .eq(params.get("designName") != null, ProductDesignEntity::getDesignName, params.get("designName"))
+                .eq(params.get("keywords") != null, ProductDesignEntity::getKeywords, params.get("keywords"))
+                .like(params.get("serviceTarget") != null, ProductDesignEntity::getServiceTarget, params.get("serviceTarget"))
+                .eq(params.get("productCategory") != null, ProductDesignEntity::getProductCategory, params.get("productCategory"));
         IPage<ProductDesignEntity> page = this.page(
                 new Query<ProductDesignEntity>().getPage(params),
-                new QueryWrapper<ProductDesignEntity>()
+                lambdaQueryWrapper
         );
 
         return new PageUtils(page);
--- course-resourse/src/main/java/com/peking/courseresourse/service/impl/ProjectReportServiceImpl.java
+++ course-resourse/src/main/java/com/peking/courseresourse/service/impl/ProjectReportServiceImpl.java
@@ -1,7 +1,11 @@
 package com.peking.courseresourse.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import dto.UserDTO;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +15,7 @@ import utils.Query;
 import com.peking.courseresourse.dao.ProjectReportDao;
 import com.peking.courseresourse.entity.ProjectReportEntity;
 import com.peking.courseresourse.service.ProjectReportService;
+import utils.UserHolder;
 
 
 @Service("projectReportService")
@@ -18,9 +23,15 @@ public class ProjectReportServiceImpl extends ServiceImpl<ProjectReportDao, Proj
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        UserDTO user = UserHolder.getUser();
+        LambdaQueryWrapper<ProjectReportEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper//.eq(ProjectReportEntity::getCreateBy,user.getId())
+                .eq(params.get("projectName") != null, ProjectReportEntity::getProjectName, params.get("projectName"))
+                .eq(params.get("keywords") != null, ProjectReportEntity::getKeywords, params.get("keywords"))
+                .eq(params.get("projectCategory") != null, ProjectReportEntity::getProjectCategory, params.get("projectCategory"));
         IPage<ProjectReportEntity> page = this.page(
                 new Query<ProjectReportEntity>().getPage(params),
-                new QueryWrapper<ProjectReportEntity>()
+                lambdaQueryWrapper
         );
 
         return new PageUtils(page);
