662a6757eba16b652a9e174934f4e4bb75a16de5
junfeng <qzhujf1012@qq.com>
2023-03-21 17:48:14 +0800

    66666

--- course-resourse/src/main/java/com/peking/courseresourse/service/impl/WeeklyReportreCordsServiceImpl.java
+++ course-resourse/src/main/java/com/peking/courseresourse/service/impl/WeeklyReportreCordsServiceImpl.java
@@ -1,7 +1,12 @@
 package com.peking.courseresourse.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.peking.courseresourse.entity.ElectronicJournalEntity;
+import dto.UserDTO;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +16,7 @@ import utils.Query;
 import com.peking.courseresourse.dao.WeeklyReportreCordsDao;
 import com.peking.courseresourse.entity.WeeklyReportreCordsEntity;
 import com.peking.courseresourse.service.WeeklyReportreCordsService;
+import utils.UserHolder;
 
 
 @Service("weeklyReportreCordsService")
@@ -18,9 +24,18 @@ public class WeeklyReportreCordsServiceImpl extends ServiceImpl<WeeklyReportreCo
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        UserDTO user = UserHolder.getUser();
+        LambdaQueryWrapper<WeeklyReportreCordsEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper//.eq(WeeklyReportreCordsEntity::getCreateBy,user.getId())
+                .eq(WeeklyReportreCordsEntity::getInternshipCommunity, params.get("internshipCommunity"))
+                .ge(params.get("internshipBegintime") != null, WeeklyReportreCordsEntity::getInternshipBegintime, params.get("internshipBegintime"))
+                .le(params.get("internshipEndtime") != null, WeeklyReportreCordsEntity::getInternshipEndtime, params.get("internshipEndtime"))
+                .eq(params.get("weeklyreportName") != null, WeeklyReportreCordsEntity::getWeeklyreportName, params.get("weeklyreportName"))
+                .eq(params.get("keywords") != null, WeeklyReportreCordsEntity::getKeywords, params.get("keywords"))
+                .like(params.get("serviceTarget") != null, WeeklyReportreCordsEntity::getServiceTarget, params.get("serviceTarget"));
         IPage<WeeklyReportreCordsEntity> page = this.page(
                 new Query<WeeklyReportreCordsEntity>().getPage(params),
-                new QueryWrapper<WeeklyReportreCordsEntity>()
+                lambdaQueryWrapper
         );
 
         return new PageUtils(page);
