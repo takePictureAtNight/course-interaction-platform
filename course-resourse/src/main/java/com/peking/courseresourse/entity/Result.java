aba4555e03ed1a89dfcfd9fa94c9032110bae40f
yaozhaoqian12 <127816168+yaozhaoqian12@users.noreply.github.com>
2023-03-25 18:20:14 +0800

    结果类

---
+++ course-resourse/src/main/java/com/peking/courseresourse/entity/Result.java
@@ -0,0 +1,10 @@
+package com.peking.courseresourse.entity;
+
+public class Result {
+    //相应码
+    private Integer code;
+    //信息
+    private String message;
+    //返回数据
+    private Object data;
+}
