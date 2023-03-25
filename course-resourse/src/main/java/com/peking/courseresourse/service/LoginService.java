b90935873380f562bf6b0a3971ea67797cca1877
yaozhaoqian12 <127816168+yaozhaoqian12@users.noreply.github.com>
2023-03-25 18:21:22 +0800

    Create LoginService.java

---
+++ course-resourse/src/main/java/com/peking/courseresourse/service/LoginService.java
@@ -0,0 +1,8 @@
+package com.peking.courseresourse.service;
+
+import com.peking.courseresourse.entity.Login;
+import com.peking.courseresourse.entity.Result;
+
+public interface LoginService {
+    public Result login(Login login);
+}
